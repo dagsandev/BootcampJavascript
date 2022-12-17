a242c75a9cff967a01791d48e1434ca076529eee
Santiago Daglio <santidaglio@gmail.com>
2022-12-17 16:58:56 -0300

    Ejercicio1Clase2

---
+++ Clase2/ejercicio.js
@@ -0,0 +1,19 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+// - Tu nombre (string)
+// - Tu edad (number)
+// - ¿Eres desarrollador? (boolean)
+// - Tu fecha de nacimiento (Date)
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const list = [
+    "Santiago",
+    29,
+    true,
+    new Date(1993,8,15),
+    {
+        titulo: "El mone que vendió su Ferrari",
+        autor: "Robin S. Sharma",
+        fecha: new Date(1996,0,1),
+        url: "https://www.amazon.es/dp/B0062XBS32/"
+    }
+]
